6232583a9e7b80d6748e601d3360b5c5e8f12bc4
victorccaldas <victor_costa_caldas@hotmail.com>
2022-04-27 00:47:27 -0300

    Update time_util.py

--- instapy/time_util.py
+++ instapy/time_util.py
@@ -34,7 +34,7 @@ def sleep(t, custom_percentage=None):
     if custom_percentage is None:
         custom_percentage = sleep_percentage
     time = randomize_time(t) * custom_percentage
-    instapy.logger.info("Sleeping about ~{}".format(time))
+    instapy.logger.info("Sleeping about ~{} seconds".format(time))
     original_sleep(time)
 
 
