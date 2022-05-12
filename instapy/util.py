310d743a88698e8c7eac0427d6e3e8efe201bb4d
victorccaldas <victor_costa_caldas@hotmail.com>
2022-05-12 20:42:58 -0300

    fix import deslogError

--- instapy/util.py
+++ instapy/util.py
@@ -37,7 +37,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
-from exceptions import DeslogError
+from .exceptions import DeslogError
 
 from .database_engine import get_database
 from .event import Event
