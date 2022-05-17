809bdf27139e9e251e42615f90d5c4bf30f8ee12
victorccaldas <victor_costa_caldas@hotmail.com>
2022-05-16 21:34:26 -0300

    update version

--- instapy/__init__.py
+++ instapy/__init__.py
@@ -1,8 +1,9 @@
 # flake8: noqa
 
 # __variables__ with double-quoted values will be available in setup.py
-__version__ = "0.6.16"
+__version__ = "0.6.17"
 # Commit 16/05/22 2:05
+print("__version__:", __version__)
 from .instapy import InstaPy
 from .util import smart_run
 from .settings import Settings
